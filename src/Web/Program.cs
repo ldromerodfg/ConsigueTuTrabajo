b189318277dcccef0bae252581460c9e4b30ecb7
Luis Romero <ldromero@diazfontanez.group>
2021-10-06 15:15:34 -0400

    KeyVault configuration

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,4 +1,9 @@
+using System;
+using Azure.Extensions.AspNetCore.Configuration.Secrets;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Service.Services;
@@ -14,6 +19,21 @@ namespace Web
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    if (context.HostingEnvironment.IsProduction())
+                    {
+                        var builtConfig = new ConfigurationBuilder()
+                            .AddJsonFile("appsettings.json", false, true)
+                            .AddEnvironmentVariables()
+                            .AddCommandLine(args)
+                            .Build();
+                        var secretClient = new SecretClient(
+                            new Uri($"https://{builtConfig["KeyVaultName"]}.vault.azure.net/"),
+                            new DefaultAzureCredential());
+                        config.AddAzureKeyVault(secretClient, new KeyVaultSecretManager());
+                    }
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
