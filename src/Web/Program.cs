2773d64b16e086009b4eef74466c5e66d5b6f54c
Luis Romero <ldromero@diazfontanez.group>
2021-10-06 14:48:53 -0400

    Revert "KeyVault configuration"
    This reverts commit ddd915bd9cfc7e962a40c3f8fc525f3fef409c13.

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -2,11 +2,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Service.Services;
-using Azure.Security.KeyVault.Secrets;
-using Azure.Identity;
-using Azure.Extensions.AspNetCore.Configuration.Secrets;
-using Microsoft.Extensions.Configuration;
-using System;
 
 namespace Web
 {
@@ -19,17 +14,6 @@ namespace Web
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((context, config) =>
-                {
-                    if (context.HostingEnvironment.IsProduction())
-                    {
-                        var builtConfig = config.Build();
-                        var secretClient = new SecretClient(
-                            new Uri($"https://{builtConfig["KeyVaultName"]}.vault.azure.net/"),
-                            new DefaultAzureCredential());
-                        config.AddAzureKeyVault(secretClient, new KeyVaultSecretManager());
-                    }
-                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
